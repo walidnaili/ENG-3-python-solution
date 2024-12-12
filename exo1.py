43304ce0b466de5ab19c62296caeccdca59eb0e2
walid naili <walidnaili998@gmail.com>
2024-12-12 18:04:40 +0100

    adding sol

--- exo1.py
+++ exo1.py
@@ -1,1 +1,9 @@
-walioddazzaza
+name = input("Please enter your name :")
+if not(name == "VIP"):
+    nb =  input("How many tickets would you like to buy ? ")
+    nb = int(nb)
+    print("The total cost is :" + str(nb * 15.50) )
+    print("Enjoy your tickets!")
+else :
+    print("Enjoy the show for free!")
+
